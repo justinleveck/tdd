b1ef2d251505f634255e06eb86214bbc9fac9dfe
Justin Leveck <jleveck@gmail.com>
2013-08-05 16:48:34 +0000

    Add merge two people and duplicate detection to Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,20 @@
 class Person < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :comapny_name, :email
-  
+  attr_accessible :first_name, :last_name, :company_name, :email
   has_many :notes
+
+  def self.merge(winner, loser)
+    winner.notes << loser.notes
+    winner.email = loser.email
+    loser.destroy
+    winner
+  end
+
+  def duplicates?(other)
+    return false if other.nil?
+    comparison_hash == other.comparison_hash
+  end
+
+  def comparison_hash
+    self.attributes.select { |attribute, value| ['first_name', 'last_name', 'email'].include? attribute.to_str }
+  end
 end
